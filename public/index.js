c71984153f4c89b940288b43f96fd24cd9f78f27
Nathan Hall <Nashing1@gmail.com>
2020-08-24 18:08:13 -0500

    added service worker and manifest
    added serviceWorker.js and manifest.json and updated index.js to use the service worker.

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/serviceWorker.js').then((reg) => {
+            console.log('Service worker registered.', reg);
+        });
+    });
+  }
+
 let transactions = [];
 let myChart;
 
---
+++ public/serviceWorker.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = "budget";
+const DATA_CACHE_NAME = "budgetName";
+
+var cachedURLs = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log("Opened cache");
+            return cache.addAll(cachedURLs);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request).then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                }).catch(err => {
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request).then(function(response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            });
+        })
+    );
+});
